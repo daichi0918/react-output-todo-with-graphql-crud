4b1d641ba7e3e950d09614dbddacc4769540eccd
daichi0918 <kayasugadaichi@icloud.com>
2025-05-20 08:57:14 +0900

    create:todo.model

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,6 +2,7 @@ import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
 import { join } from 'path';
+import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { join } from 'path';
       playground: true,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
     }),
+    TodoModule,
   ],
 })
 export class AppModule {}
