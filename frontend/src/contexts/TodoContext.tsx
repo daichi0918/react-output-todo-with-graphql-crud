03eb8edec89acef51a98bcf657f3d6fb02c58b75
daichi0918 <kayasugadaichi@icloud.com>
2025-12-07 11:16:37 +0900

    TodoContext修正

--- frontend/src/contexts/TodoContext.tsx
+++ frontend/src/contexts/TodoContext.tsx
@@ -1,8 +1,8 @@
 'use client';
 
-import { useQuery } from '@apollo/client';
 // import { INITIAL_TODO_LIST, INITIAL_TODO_LIST_LENGTH } from '@/constants/data';
-import type { TodoType } from '../type';
+import type { Todo } from '../gql/graphql';
+import { useGetTodosQuery } from '../gql/graphql';
 import React, {
   type FC,
   type ReactNode,
@@ -11,15 +11,14 @@ import React, {
   useMemo,
   useState,
 } from 'react';
-import { GET_TODOS } from '../queries/todoQueries';
 
 interface Props {
   children: ReactNode;
 }
 
 interface TodoContextInterface {
-  originalTodoList: Array<TodoType>;
-  setOriginalTodoList: React.Dispatch<React.SetStateAction<Array<TodoType>>>;
+  originalTodoList: Array<Todo>;
+  setOriginalTodoList: React.Dispatch<React.SetStateAction<Array<Todo>>>;
   todoListLength: number;
   setTodoListLength: React.Dispatch<React.SetStateAction<number>>;
 }
@@ -27,7 +26,7 @@ interface TodoContextInterface {
 export const TodoContext = createContext({} as TodoContextInterface);
 
 export const TodoProvider: FC<Props> = ({ children }) => {
-  const [originalTodoList, setOriginalTodoList] = useState<Array<TodoType>>([]);
+  const [originalTodoList, setOriginalTodoList] = useState<Array<Todo>>([]);
   const [todoListLength, setTodoListLength] = useState<number>(0);
 
   const contextValue = useMemo(
@@ -40,16 +39,14 @@ export const TodoProvider: FC<Props> = ({ children }) => {
     [originalTodoList, todoListLength] // `originalTodoList`が変わった場合のみ再生成
   );
 
-  const { data, loading, error } = useQuery<{ getTodos: TodoType[] }>(
-    GET_TODOS
-  );
+  const { data, loading, error } = useGetTodosQuery();
 
   useEffect(() => {
     if (loading || error) return;
-    if (data?.getTodos && Array.isArray(data.getTodos)) {
-      setOriginalTodoList(data.getTodos);
-      setTodoListLength(data.getTodos.length);
-    }
+    const todos =
+      data?.getTodos?.filter((todo): todo is Todo => Boolean(todo)) ?? [];
+    setOriginalTodoList(todos);
+    setTodoListLength(todos.length);
   }, [data, error, loading]);
   return (
     <TodoContext.Provider value={contextValue}>{children}</TodoContext.Provider>
